f27138e873d8f3d8ad663499438e18eb029a3fb4
Lando Vargas <lando_vargas@outlook.com>
2022-07-25 03:02:32 -0400

    Cleared an undesired console.log

--- matchtwo.js
+++ matchtwo.js
@@ -180,12 +180,11 @@ class Table
                 
                 if(seconds < 10)
                     seconds = `0${seconds}`;
-                
                 if(minutes < 10)
                     minutes = `0${minutes}`;
                 if(hours < 10)
                     hours = `0${hours}`;
-                console.log(seconds)
+                
                 timeString = `${hours}:${minutes}:${seconds} hours`;
             }
             labelTime.innerText = `Run started ${timeString} ago`
